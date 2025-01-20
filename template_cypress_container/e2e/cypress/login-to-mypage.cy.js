e95360e9900a32ee473c4214d5f3f920cf0dc2a0
dbd-fish <ryo.kokai.g@gmail.com>
2025-01-20 14:18:54 +0900

    Cypressテストを修正

--- template_cypress_container/e2e/cypress/login-to-mypage.cy.js
+++ template_cypress_container/e2e/cypress/login-to-mypage.cy.js
@@ -1,17 +1,30 @@
 describe('ログインテスト', () => {
+
+
+  let loginData;
+  before(() => {
+    // テストデータを読み込む
+    cy.fixture('loginData').then((data) => {
+      loginData = data;
+    });
+  });
+
+
   it('フォームに正しく入力して送信', () => {
     cy.visit('/login');
 
+    // NOTE: これがないと以降の操作ができない
     // ページの読み込みを待つ
     cy.wait(1000); // または適切な時間
 
+    // NOTE: ここで正しいemailとパスワードの組み合わせは別コンテナを参照する必要がある
     cy.get('input#email')
       .should('be.enabled') // 要素が有効になるまで待機
-      .type('user@example.com');
+      .type(loginData.correctEmail);
 
     cy.get('input#password')
       .should('be.enabled')
-      .type('securepassword');
+      .type(loginData.correctPassword);
 
     cy.get('button[type="submit"]').click();
     cy.wait(1000); // または適切な時間
@@ -32,19 +45,21 @@ describe('ログインテスト', () => {
   it('ホーム画面からログアウト画面へ遷移', () => {
     cy.visit('/login');
 
+    // NOTE: これがないと以降の操作ができない
     // ページの読み込みを待つ
     cy.wait(1000); // または適切な時間
 
+    // NOTE: ここで正しいemailとパスワードの組み合わせは別コンテナを参照する必要がある
     cy.get('input#email')
       .should('be.enabled') // 要素が有効になるまで待機
-      .type('user@example.com');
+      .type(loginData.correctEmail);
 
     cy.get('input#password')
       .should('be.enabled')
-      .type('securepassword');
+      .type(loginData.correctPassword);
 
     cy.get('button[type="submit"]').click();
-
+    cy.wait(1000); // または適切な時間
     // マイページ画面に遷移
     cy.url().should('eq', 'https://frontend:5173/mypage');
 
