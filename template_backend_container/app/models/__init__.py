a8cf59776cb8ccfc542d672412fe566779fcfa31
dbd-fish <ryo.kokai.g@gmail.com>
2025-01-22 22:08:08 +0900

    WIP: 戻す可能性があるが一時的にコミット

--- template_backend_container/app/models/__init__.py
+++ template_backend_container/app/models/__init__.py
@@ -1,26 +1,7 @@
 # app/models/__init__.py
 
 # 各モデルをインポート
-from .group_evaluation import GroupEvaluation
-from .group_evaluation_history import GroupEvaluationHistory
-from .group_profile import GroupProfile
-from .group_search_history import GroupSearchHistory
-from .report import Report
-from .report_comment_history import ReportCommentHistory
-from .report_evaluation_history import ReportEvaluationHistory
-from .report_supplement import ReportSupplement
-from .report_tag import ReportTag
-from .report_tag_link import ReportTagLink
-from .report_view_history import ReportViewHistory
-from .tag_view_history import TagViewHistory
 from .user import User
-from .user_evaluation_history import UserEvaluationHistory
-from .user_group import UserGroup
-from .user_group_membership import UserGroupMembership
-from .user_ip_address import UserIPAddress
-from .user_profile import UserProfile
-from .user_search_history import UserSearchHistory
-from .user_view_history import UserViewHistory
 
 # 他のモデルがある場合も同様に追加
 # from .other_model import OtherModel
--- template_backend_container/app/routes.py
+++ template_backend_container/app/routes.py
@@ -3,16 +3,12 @@ from fastapi import APIRouter
 from app.config.setting import setting
 from app.controllers.auth_controller import router as auth_router
 from app.controllers.dev_controller import router as dev_router
-from app.controllers.report_controller import router as report_router
 
 router = APIRouter()
 
 if setting.DEV_MODE:
     # 開発用のルーター定義
     router.include_router(dev_router, prefix="/api/dev",  tags=["dev"])
-
-# レポート用のルーター定義
-router.include_router(report_router, prefix="/api/report",  tags=["report"])
 
 # 認証用のルーター
 router.include_router(auth_router, prefix="/api/auth", tags=["auth"])
