31d786a79a6244da25bc570b7135f257907c2cf7
dbd-fish <ryo.kokai.g@gmail.com>
2025-01-23 20:31:36 +0900

    テーブル削除の修正もれ

--- template_backend_container/app/models/__init__.py
+++ template_backend_container/app/models/__init__.py
@@ -9,24 +9,5 @@ from .user import User
 # Alembicや他のスクリプトがapp.modelsをインポートするだけで全モデルを認識できるようにする
 __all__ = [
     "user",
-    "user_profile",
-    "user_ip_address",
-    "user_group",
-    "group_profile",
-    "user_group_membership",
-    "report",
-    "report_tag",
-    "report_tag_link",
-    "report_supplement",
-    "user_evaluation_history",
-    "report_evaluation_history",
-    "group_evaluation_history",
-    "report_comment_history",
-    "tag_view_history",
-    "group_evaluation",
-    "report_view_history",
-    "user_view_history",
-    "user_search_history",
-    "group_search_history",
     # "OtherModel"  # 他のモデルを追加する場合もここに名前を追加
 ]
