999cfe8288e3ef25c420282cc976ffd962118283
dbd-fish <ryo.kokai.g@gmail.com>
2025-01-25 20:32:36 +0900

    seederの細分化

--- template_backend_container/app/features/feature_dev/seed_data.py
+++ template_backend_container/app/features/feature_dev/seed_data.py
@@ -4,17 +4,13 @@
 # export PYTHONPATH=/app
 # poetry run python app/seeders/seed_data.py
 
-import asyncio
 
 from passlib.context import CryptContext  # type: ignore
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
-from sqlalchemy.future import select
 from sqlalchemy.sql import text
 
-import app.models
-from app.common.common import datetime_now
-from app.config.test_data import TestData
 from app.common.database import AsyncSessionLocal, Base
+from app.features.feature_dev.seed_user import seed_user
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -52,73 +48,10 @@ async def seed_data(db: AsyncSession):
 
     async with AsyncSessionLocal(bind=engine) as session:
         try:
-            # 1. Userテーブル
-            result = await session.execute(
-                select(app.models.User).where(app.models.User.username == TestData.TEST_USERNAME_1),
-            )
-            if not result.scalars().first():
-                session.add(
-                    app.models.User(
-                        user_id=TestData.TEST_USER_ID_1,
-                        username=TestData.TEST_USERNAME_1,
-                        email=TestData.TEST_USER_EMAIL_1,
-                        hashed_password=pwd_context.hash(TestData.TEST_USER_PASSWORD),
-                        contact_number=TestData.TEST_USER_CONTACT_1,
-                        user_role=1,
-                        user_status=1,
-                        created_at=datetime_now(),
-                        updated_at=datetime_now(),
-                    ),
-                )
-            await session.commit()
-
-            # 1. Userテーブル (target_user_id 用のデータ追加)
-            result = await session.execute(
-                select(app.models.User).where(app.models.User.username == TestData.TEST_USERNAME_2),
-            )
-            if not result.scalars().first():
-                session.add(
-                    app.models.User(
-                        user_id=TestData.TEST_USER_ID_2,
-                        username=TestData.TEST_USERNAME_2,
-                        email=TestData.TEST_USER_EMAIL_2,
-                        hashed_password=pwd_context.hash(TestData.TEST_USER_PASSWORD),
-                        contact_number=TestData.TEST_USER_CONTACT_2,
-                        user_role=2,
-                        user_status=1,
-                        created_at=datetime_now(),
-                        updated_at=datetime_now(),
-                    ),
-                )
-            await session.commit()
-            print("Data seeded successfully!")
+            await seed_user(session)
 
         except Exception as e:
             await session.rollback()
             print(f"An error occurred: {e}")
         finally:
             await session.close()
-
-
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(description="Seed or clear database.")
-    parser.add_argument("--clear", action="store_true", help="Clear all database data")
-    parser.add_argument("--seed", action="store_true", help="Seed the database with initial data")
-    args = parser.parse_args()
-
-    async def main():
-        if args.clear:
-            print("Clearing database...")
-            await clear_data()
-        elif args.seed:
-            print("Seeding database...")
-            await seed_data()
-        else:
-            # 引数なしの場合、両方を実行
-            print("No arguments provided. Clearing and seeding database...")
-            await clear_data()
-            await seed_data()
-
-    asyncio.run(main())
